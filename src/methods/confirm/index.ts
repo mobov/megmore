8d6f6096bdaff01a2b9a92f9457feddd2636ddf5
wenwenhai <gaarashura0@gmail.com>
2018-07-01 12:52:19 +0800

    弹窗调整

--- src/methods/confirm/index.ts
+++ src/methods/confirm/index.ts
@@ -1,37 +1,37 @@
 import MModal from '@/components/modal'
 import { Component, Vue } from 'vue-property-decorator'
-import { ComponentOptions } from 'vue';
+import { ComponentOptions, VNode } from 'vue';
 import { Model } from '@/types'
 
 @Component
 class Confirm extends MModal {
- public resolve!: () => void
- public reject!: () => void
- public title!: string
- public confirm() {
-  MModal.options.methods.confirm.call(this)
-  this.resolve()
- }
- public cancel() {
-  MModal.options.methods.cancel.call(this)
-  this.reject()
- }
- public afterLeave() {
-  this.$destroy()
- }
+  public resolve!: () => void
+  public reject!: () => void
+  public title!: string
+  public confirm() {
+    MModal.options.methods.confirm.call(this)// hack super.confirm()
+    this.resolve()
+  }
+  public cancel() {
+    MModal.options.methods.cancel.call(this)
+    this.reject()
+  }
+  public afterLeave() {
+    this.$destroy()
+  }
 }
 
 const confirm = async (options: Model.ConfirmOptions = { title: '', content: '' }) => {
- return new Promise(async (resolve, reject) => {
-  const instance = new Confirm()
-  instance.title = options.title as string
-  instance.content = options.content as (string | Model.Render)
-  instance.resolve = resolve
-  instance.reject = reject
-  instance.$mount()
-  instance.domExist = true
-  await instance.$nextTick()
-  instance.selfShow = true
- })
+  return new Promise(async (resolve, reject) => {
+    const instance = new Confirm()
+    instance.title = options.title as string
+    instance.content = options.content as (string | VNode)
+    instance.resolve = resolve
+    instance.reject = reject
+    instance.$mount()
+    instance.domExist = true
+    await instance.$nextTick()
+    instance.visible = true
+  })
 }
 export default confirm
