7d24ffc9c52e9532c4a56ad60b0b785b22336bfa
wenwenhai <gaarashura0@gmail.com>
2018-07-22 21:47:41 +0800

    textarea

--- src/components/text-field/input.tsx
+++ src/components/text-field/input.tsx
@@ -10,19 +10,16 @@ export default class MTextField extends Vue {
     })
     private value!: string
     @Prop({
-        default: 'Label'
+        default: ''
     })
     private label!: string
+
     @Prop({
-        default: 'xl'
-    })
-    private size!: string
-    @Prop({
-        default: 'menu'
+        default: ''
     })
     private icon!: string
     @Prop({
-        default: 'menu'
+        default: ''
     })
     private endIcon!: string
     @Prop({
@@ -31,7 +28,7 @@ export default class MTextField extends Vue {
     })
     private labelFloat!: boolean
     @Prop({
-        default: 'asdasdasdasd'
+        default: ''
     })
     private helpText!: string
     @Prop({
@@ -43,6 +40,27 @@ export default class MTextField extends Vue {
         type: Boolean
     })
     private disabled!: boolean
+    @Prop({
+        default: '',
+        type: String
+    })
+    private prefix!: string
+
+    @Prop({
+        default: '',
+        type: String
+    })
+    private suffix!: string
+    @Prop({
+        default: false,
+        type: Boolean
+    })
+    private multiLine!: boolean
+    @Prop({
+        default: 3,
+        type: Number
+    })
+    private rows!: number
 
     private isFocus: boolean = false
 
@@ -65,6 +83,18 @@ export default class MTextField extends Vue {
         return this.endIcon !== ''
     }
 
+    private get hasPrefix() {
+        return this.prefix !== ''
+    }
+
+    private get hasSuffix() {
+        return this.suffix !== ''
+    }
+
+    private get hasText() {
+        return this.text !== ''
+    }
+
     private handleFocus() {
         if (this.disabled) {
             return
@@ -77,28 +107,28 @@ export default class MTextField extends Vue {
 
 
     private handleBlur() {
-        if (!this.text)
-            this.isFocus = false
+        this.isFocus = false
     }
-    @Watch('text',{
+
+    @Watch('text', {
         immediate: true
     })
-    private handleInput(val:string){
-        this.$emit('input',val)
+    private handleInput(val: string) {
+        this.$emit('input', val)
     }
+
     private render(h: any) {
         const labelCls = {
-            'm--active': this.isFocus,
-            [`m--${this.size}`]: true,
+            'm--active': this.isFocus || this.hasText,
             'm--label-float': this.labelFloat,
+            'm--has-icon': this.hasIcon,
         }
         const wrapCls = {
             'm--active': this.isFocus,
-            'm--has-icon': this.hasIcon,
             'm--disabled': this.disabled,
 
         }
-        const inputCls = {
+        const inputWrapCls = {
             'm--has-end-icon': this.hasEndIcon
         }
         const contenCls = {
@@ -112,54 +142,73 @@ export default class MTextField extends Vue {
             'm--bg-danger': this.hasErrorText,
             'm--bg-primary': !this.hasErrorText,
         }
-        let iconCls:any={
-            'm--color-danger': this.isFocus&&this.hasErrorText,
-            'm--color-primary': this.isFocus&&!this.hasErrorText,
+        let iconCls: any = {
+            'm--color-danger': this.isFocus && this.hasErrorText,
+            'm--color-primary': this.isFocus && !this.hasErrorText,
         }
-        iconCls=Object.keys(iconCls).map((key:string)=>{
-            if(iconCls[key]===true){
+        iconCls = Object.keys(iconCls).map((key: string) => {
+            if (iconCls[key] === true) {
                 return key
-            }else{
+            } else {
                 return ''
             }
         })
-        iconCls=iconCls.join(' ')
+        iconCls = iconCls.join(' ')
         console.log(iconCls)
         return (
             <div staticClass='m-text-field' class={wrapCls}>
-                {this.hasIcon && (<MIcon name={this.icon} />)}
-                <div staticClass='m-text-field__label' class={labelCls} onClick={this.handleFocus}>
-                    {this.label}
-                </div>
+                {this.hasIcon && (<MIcon name={this.icon}/>)}
+
                 <div staticClass='m-text-field__content' class={contenCls}>
-                    <input
-                        disabled={this.disabled}
-                        ref="input"
-                        v-model={this.text}
-                        type="text"
-                        staticClass='m-text-field__input'
-                        class={inputCls}
-                        onFocus={this.handleFocus}
-                        onBlur={this.handleBlur}/>
-                    {this.hasEndIcon && (<MIcon class={iconCls} name={this.endIcon} />)}
-                </div>
-                <div staticClass='m-text-field__underline' class={underlineCls}>
-                    <div
-                        staticClass='m-text-field__underline-content m--bg-m-text-field__underline-content'
-                        class={underlineContentCls} />
-                </div>
-
-                {!this.hasErrorText && this.hasHelpText && (
-                    <div class='m-text-field__help-text'>
-                        {this.helpText}
+                    <div   staticClass='m-text-field__label' class={labelCls} onClick={this.handleFocus}>
+                        {this.label}
                     </div>
-                )}
-                {this.hasErrorText && (
-                    <div class='m-text-field__error-text m--color-danger'>
-                        {this.helpText}
-                    </div>
-                )}
+                    <div staticClass='m-text-field__input-wrap' class={inputWrapCls}>
+                        {
+                            this.hasPrefix &&
+                            (<span staticClass='m-text-field__suffix'
+                                   v-show={this.isFocus || this.hasText}>{this.prefix}</span>)
+                        }
 
+                        {!this.multiLine ?
+                            (
+                                <input
+                                    disabled={this.disabled}
+                                    ref="input"
+                                    v-model={this.text}
+                                    type="text"
+                                    staticClass='m-text-field__input'
+                                    onFocus={this.handleFocus}
+                                    onBlur={this.handleBlur}/>
+                            ) : (
+                                <textarea
+                                    ref="input"
+                                    v-model={this.text}
+                                    onFocus={this.handleFocus}
+                                    onBlur={this.handleBlur}
+                                    staticClass='m-text-field__input'
+                                    rows={this.rows}/>
+                            )}
+
+                        {this.hasSuffix && (<span staticClass='m-text-field__suffix'>{this.suffix}</span>)}
+                    </div>
+                    {this.hasEndIcon && (<MIcon class={iconCls} name={this.endIcon}/>)}
+                    <div staticClass='m-text-field__underline' class={underlineCls}>
+                        <div
+                            staticClass='m-text-field__underline-content m--bg-m-text-field__underline-content'
+                            class={underlineContentCls}/>
+                    </div>
+                    {!this.hasErrorText && this.hasHelpText && (
+                        <div class='m-text-field__help-text'>
+                            {this.helpText}
+                        </div>
+                    )}
+                    {this.hasErrorText && (
+                        <div class='m-text-field__error-text m--color-danger'>
+                            {this.helpText}
+                        </div>
+                    )}
+                </div>
             </div>
         )
     }
