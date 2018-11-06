5577422a6ae81172b75fd856417d2611509bebad
wenwenhai <gaarashura0@gmail.com>
2018-11-06 14:23:16 +0800

    🍠过度动画调整

--- src/components/expansion-panel/expansion-panel-content.tsx
+++ src/components/expansion-panel/expansion-panel-content.tsx
@@ -3,6 +3,7 @@ import { mixins } from 'vue-class-component'
 import toggleable from '@/mixins/toggleable'
 import { addOnceEventListener } from '@/utils/dom'
 import Icon from '@/components/icon'
+import {MTransitionExpansion} from '@/components/transition'
 @Component
 export default class MExpansionPanelContent extends mixins(toggleable) {
   @Inject()
@@ -21,39 +22,7 @@ export default class MExpansionPanelContent extends mixins(toggleable) {
   private mounted() {
     this.contentInit(this)
   }
-  private beforeDestory() {
-    this.contentRemove(this)
-  }
 
-  private async enter(el: HTMLElement, done: () => void) {
-    el.style.overflow = 'hidden'
-    el.style.height = '0'
-    el.style.display = 'block'
-    addOnceEventListener(el, 'transitionend', done)
-
-    // el.style.height = String(el.offsetHeight) + 'px'
-    console.log(el.scrollHeight)
-    el.style.height = `${el.scrollHeight}px`
-
-  }
-  private leave(el, done) {
-    // Remove initial transition
-    addOnceEventListener(el, 'transitionend', done)
-
-    // Set height before we transition to 0
-    el.style.overflow = 'hidden'
-    el.style.height = `${el.offsetHeight}px`
-    setTimeout(() => (el.style.height = 0), 100)
-    // Promise.resolve().then(() => {
-    //   el.style.height = 0
-    // })
-  }
-  private afterEnter(el: HTMLElement) {
-    el.style.display = null
-    el.style.height = null
-    el.style.overflow = null
-    console.log(el.style)
-  }
   private render(h) {
     const iconcls = this.visible
         ? 'm-expansion-panel-content__header-icon--active'
@@ -67,13 +36,13 @@ export default class MExpansionPanelContent extends mixins(toggleable) {
             ||
             (<Icon name='arrow_drop_down' class={`m-expansion-panel-content__header-icon ${iconcls}`}></Icon>)}
         </div>
-        <transition name='m-expansion-panel' onEnter={this.enter} onAfterEnter={this.afterEnter} onLeave={this.leave}>
+        <MTransitionExpansion >
           <div class='m-expansion-panel-content__body' v-show={this.visible} >
             <div class='m-expansion-panel-content__body-content'  >
               {this.$slots.default}
             </div>
           </div>
-        </transition>
+        </MTransitionExpansion>
       </div>
     )
   }
